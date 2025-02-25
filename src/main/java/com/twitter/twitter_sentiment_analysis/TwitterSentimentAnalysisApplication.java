4fdede93af05d68e270a23c0499e10d5811daf81
charigardash <charigardash@gmail.com>
2025-02-26 00:14:01 +0530

    project setup

---
+++ src/main/java/com/twitter/twitter_sentiment_analysis/TwitterSentimentAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.twitter_sentiment_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSentimentAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterSentimentAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitter/twitter_sentiment_analysis/TwitterSentimentAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.twitter_sentiment_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterSentimentAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
